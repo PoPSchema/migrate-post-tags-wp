0539bec1245408204f758788a233c82e38509c2a
Leonardo Losoviz <leo@getpop.org>
2020-07-18 07:54:17 +0800

    Provide taxonomy in service

--- migrate/contractimplementations/cms-apis/cms-functionapi.php
+++ migrate/contractimplementations/cms-apis/cms-functionapi.php
@@ -2,11 +2,30 @@
 
 namespace PoP\PostTags\WP;
 
-class FunctionAPI extends \PoP\Tags\WP\FunctionAPI implements \PoP\PostTags\FunctionAPI
+class FunctionAPI extends \PoP\Tags\WP\AbstractFunctionAPI implements \PoP\PostTags\FunctionAPI
 {
     public function __construct()
     {
         \PoP\PostTags\FunctionAPIFactory::setInstance($this);
+    }
+
+    /**
+     * Implement this function by the actual service
+     *
+     * @return string
+     */
+    protected function getTaxonomyName(): string
+    {
+        return 'post_tag';
+    }
+    /**
+     * Implement this function by the actual service
+     *
+     * @return string
+     */
+    protected function getTagBaseOption(): string
+    {
+        return 'tag_base';
     }
 }
 
