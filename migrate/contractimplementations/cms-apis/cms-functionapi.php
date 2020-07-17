b135d30573843674c5bbd03e5f53b5201bb15e49
Leonardo Losoviz <leo@getpop.org>
2020-07-18 07:34:07 +0800

    Fixed instantiating service

--- migrate/contractimplementations/cms-apis/cms-functionapi.php
+++ migrate/contractimplementations/cms-apis/cms-functionapi.php
@@ -2,9 +2,12 @@
 
 namespace PoP\PostTags\WP;
 
-class FunctionAPI extends \PoP\Tags\WP\FunctionAPI
+class FunctionAPI extends \PoP\Tags\WP\FunctionAPI implements \PoP\PostTags\FunctionAPI
 {
-
+    public function __construct()
+    {
+        \PoP\PostTags\FunctionAPIFactory::setInstance($this);
+    }
 }
 
 /**
